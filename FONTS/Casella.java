329dc7a16167ef97e542870652d50011bb1cb869
JoanDomingo <joan.domingo.navarro@estudiantat.upc.edu>
2020-10-29 10:52:54 +0100

    modif casella

--- FONTS/Casella.java
+++ FONTS/Casella.java
@@ -2,36 +2,10 @@ import java.io.*;
 import java.util.*;
 
 public class Casella {
-    int posX;
-    int posY;
 
     //public functions
 
     //creadores
-    public Casella (int x, int y){
-        this.posX = x;
-        this.posY = y;
-    }
-
-    //modificadores
-    public void setPosX (int x) {
-        this.posX = x;
-    }
-
-    public void setPosY (int y) {
-        this.posY = y;
-    }
-
-    //consultores
-    public int getposX () { //consulta posX
-        return posX;
-    }
-
-    public int getposY () { //consulta posY
-        return posY;
-    }
-
-
-
+    public Casella (){}
 
 }

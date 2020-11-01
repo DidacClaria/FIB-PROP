34b9ca401fdaf924b53a9e2b0b899e36d3b4cd6e
DidacClaria <dclariah99@gmail.com>
2020-11-01 18:19:30 +0100

    Nova Estructura Projecte

---
+++ FONTS/DOMINI/Estadistica.java
@@ -0,0 +1,16 @@
+package DOMINI;
+
+public class Estadistica {
+
+    private int temps;
+
+    private int numAjudes;
+
+    private int puntació;
+
+    private boolean automatic;
+
+    private boolean completat;
+
+    private KakuroPle kakuroPle;
+}

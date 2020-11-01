9e3331306024bd1216e9a950281a8f48bc0fce02
DidacClaria <dclariah99@gmail.com>
2020-11-01 18:19:30 +0100

    Nova Estructura Projecte

---
+++ FONTS/DOMINI/KakuroPle.java
@@ -0,0 +1,20 @@
+package DOMINI;
+
+public class KakuroPle extends PlantillaKakuro{
+
+    private Estadistica estadistica;
+
+    private int id_k;
+
+    public void jugarKakuro(){
+
+    }
+
+    public void demanarAjutKakuro(){
+
+    }
+
+    public void guardarKakuro() {
+
+    }
+}

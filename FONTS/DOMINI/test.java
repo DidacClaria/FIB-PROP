34f10d8cad7ff9410cd75d00db572e4fc8dc5c98
long.chen <long.chen@est.fib.upc.edu>
2020-11-09 19:39:46 +0100

    Nova versió de read y print

--- FONTS/DOMINI/test.java
+++ FONTS/DOMINI/test.java
@@ -3,9 +3,12 @@ package DOMINI;
 public class test {
 
     public static void main (String[] args) {
+
         prova p = new prova ();
         p.read_kakuro();
         boolean solució = p.solve_kakuro();
         p.print_kakuro();
+
+
     }
 }

0631ab212dcbbef6ade453c19a653c8a1922f7df
long.chen <long.chen@est.fib.upc.edu>
2020-11-12 11:47:05 +0100

    Versió generator

---
+++ FONTS/DOMINI/Test.java
@@ -0,0 +1,18 @@
+package DOMINI;
+
+public class Test {
+    public static void main (String[] args) {
+
+        /*
+        Prova p = new Prova ();
+        p.read_kakuro();
+        boolean s = p.solve_kakuro();
+        p.print_kakuro();
+        */
+
+        Prova p = new Prova (20, 20);
+        p.print_kakuro();
+
+    }
+
+}
